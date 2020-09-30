268238b82cb15a7b1cfdbaedf36ac7c0123617b1
Nataliia Ponomarenko <natasha132354@gmail.com>
2020-09-30 23:57:15 +0300

    initial commit

---
+++ Converter/Converter/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double usdPrice;
+            double eurPrice;
+            int option;
+            double money;
+            Console.WriteLine("Введіть курс долару та євро");
+            usdPrice = Convert.ToDouble(Console.ReadLine());
+            eurPrice = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Оберіть функцію: \n1. Перевести гривні в долари \n2. Перевести долари в гривні \n3. Перевести гривні в євро \n4. Перевести євро в гривні");
+            option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть суму валюти");
+            money = Convert.ToDouble(Console.ReadLine());
+            Converter conv = new Converter(usdPrice, eurPrice);
+            if (option == 1)
+            {
+                Console.WriteLine(conv.uahToUsd(money).ToString() + " USD");
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine(conv.usdToUah(money).ToString() + " UAH");
+            }
+            else if (option == 3)
+            {
+                Console.WriteLine(conv.uahToEur(money).ToString() + " EUR");
+            }
+            else {
+                Console.WriteLine(conv.eurToUah(money).ToString() + " UAH");
+            }
+        }
+    }
+
+    class Converter
+    {
+        //private double uah;
+        private double usdPrice;
+        private double eurPrice;
+        public Converter (double usd, double eur)
+        {
+            usdPrice = usd;
+            eurPrice = eur;
+        }
+        public double uahToUsd(double money)
+        {
+            return (money / usdPrice);
+        }
+        public double usdToUah(double money)
+        {
+            return (money* usdPrice);
+        }
+        public double uahToEur(double money)
+        {
+            return (money / eurPrice);
+        }
+        public double eurToUah(double money)
+        {
+            return (money * eurPrice);
+        }
+    }
+}
---
+++ Students/Students/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student("Klara");
+            GoodStudent st2 = new GoodStudent("Vlad");
+            BadStudent st3 = new BadStudent("Nataliia");
+            Group group1 = new Group("K-24");
+            group1.AddStudent(st1);
+            group1.AddStudent(st3);
+            group1.AddStudent(st2);
+            st1.Study();
+            st2.Study();
+            st3.Study();
+            Console.Write(group1.GetInfo());
+            Console.Write(group1.GetFullInfo());
+        }
+    }
+
+    class Student
+    {
+        private string name;
+        protected string state;
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public Student(string name)
+        {
+            this.name = name;
+            state = "";
+        }
+
+        public string getState()
+        {
+            return state;
+        }
+
+        public void Relax()
+        {
+            state += "Relax ";  
+        }
+        public void Read()
+        {
+            state += "Read ";
+        }
+        public void Write()
+        {
+            state += "Write ";
+        }
+
+        public virtual void Study()
+        {
+
+        }
+    }
+
+    class GoodStudent : Student
+    {
+        public GoodStudent(string name) : base(name) 
+        {
+            state += "good ";
+        }
+        public override void Study()
+        {
+            Read();
+            Write(); 
+            Read(); 
+            Write(); 
+            Relax();
+        }
+    }
+
+    class BadStudent : Student
+    {
+        public BadStudent(string name) : base(name)
+        {
+            state += "bad ";
+        }
+        public override void Study()
+        {
+            Relax(); 
+            Relax(); 
+            Relax(); 
+            Relax(); 
+            Read();
+        }
+    }
+
+    class Group
+    {
+        private string nameGroup;
+        private List<Student> studentsList;
+        public Group(string name)
+        {
+            studentsList = new List<Student>();
+            nameGroup = name;
+        }
+
+        public void AddStudent(Student st)
+        {
+            studentsList.Add(st);
+        }
+        public string GetInfo()
+        {
+            string result = "";
+            result += nameGroup + " :\n";
+            foreach (Student s in studentsList)
+            { 
+                result += s.getName() + "\n";
+            }
+            return result;
+        }
+
+        public string GetFullInfo()
+        {
+            string result = "";
+            result += nameGroup + " :\n";
+            foreach (Student s in studentsList)
+            {
+                result += s.getName() + " - " + s.getState() + "\n";
+            }
+            return result;
+        }
+
+    }
+}
